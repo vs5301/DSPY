92cbd5f06e758031fbac6bf390c63903ef5ab74f
Vaibhav Sharma <vaibhav.sharma5301@gmail.com>
2023-11-25 13:08:25 +0530

    Session9

---
+++ Session9C.py
@@ -0,0 +1,20 @@
+app_name = "WhatsApp"
+users = 1000
+
+print(">> Welcome to", app_name)
+print(">> Welcome to %s" % app_name)
+
+print(">> Welcome to", app_name, "We got", users, "users")
+print(">> Welcome to {} We got {} users".format(app_name, users))
+
+name = "John"
+email = "john@example.com"
+password = "pass123"
+
+sql = "insert into Customer values('{}', '{}', '{}')".format(name, email, password)
+print(sql)
+
+num1 = 10
+num2 = 10/3
+
+print("Result is: {}".format(num2))
